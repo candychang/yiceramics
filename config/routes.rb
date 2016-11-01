3ff1b34166c0c5f96cd9a98dc8e901215fda48ae
Candy Chang <candyichang@gmail.com>
2016-11-01 20:57:05 +0000

    basic image upload complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   
   resources :posts
+  resources :works
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027213449) do
+ActiveRecord::Schema.define(version: 20161101193229) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20161027213449) do
     t.string   "fire_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
