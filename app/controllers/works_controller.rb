dc7c1d064f0264911c46bf77a6efbc0383556133
Candy Chang <candyichang@gmail.com>
2016-11-01 20:57:05 +0000

    basic image upload complete

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,42 @@
+class WorksController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def index
+      @works = Work.all
+  end
+  
+  def show
+      @work = Work.find_by_id(params[:id])
+  end
+  
+  def new
+      @work = Work.new
+  end
+  
+  def create
+      @work = Work.create(work_params)
+      if @work.save!
+          redirect_to @work
+      else
+          //
+      end
+  end
+  
+  def update
+      curr_work = Work.find_by_id(params[:id])
+      if curr_work.update!(work_params)
+          redirect_to curr_work
+      else
+          //
+      end
+  end
+  
+  private
+    def work_params
+      params.require(:work).permit(:name, :details, :size, :clay_type, :fire_type, :image, :remove_image)
+    end
+  
+  
+end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -32,15 +32,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [100, 100]
+  end
+  
+  version :gallery do
+    process :resize_to_fit => [300, 300]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
