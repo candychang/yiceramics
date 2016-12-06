06fa2e8bbbdf11ccece4ec57437c18a49c8deefe
Candy Chang <candyichang@gmail.com>
2016-12-06 05:02:42 +0000

    clear cart feature added, updated tests

--- app/assets/javascripts/shoppingCart.js
+++ app/assets/javascripts/shoppingCart.js
@@ -4,14 +4,10 @@ function ShoppingCart(cartName) {
         if (!cart) {
             this.name = cartName;
             this.items = [];
-            this.totalCount = 0;
-            this.totalPrice = 0.00;
             storeWithExpiration.set(cartName, this);
         } else {
             this.name = cart.name;
             this.items = cart.items;
-            this.totalCount = cart.totalCount;
-            this.totalPrice = cart.totalPrice;
         }
     } else {
         alert('Local storage is not supported by your browser - the shopping cart will not work. Please disable "Private Mode", or upgrade to a modern browser.')
@@ -20,8 +16,6 @@ function ShoppingCart(cartName) {
 
 ShoppingCart.prototype.clearCart = function() {
     this.items = [];
-    this.totalCount = 0;
-    this.totalPrice = 0.00;
     storeWithExpiration.set(this.name, this);
 }
 
@@ -31,8 +25,6 @@ ShoppingCart.prototype.addItem = function(id, name, price, quantity, img) {
     if (index === -1 && quantity > 0) {
         item = { work_id: id, name: name, price: price, quantity: quantity, imageURL: img };
         this.items.push(item);
-        this.totalCount += quantity;
-        this.totalPrice += quantity * item.price;
         storeWithExpiration.set(this.name, this);
     }
 }
@@ -46,8 +38,6 @@ ShoppingCart.prototype.removeItemByIndex = function(index) {
     if (index >= 0) {
         var item = this.items[index];
         this.items.splice(index, 1);
-        this.totalCount -= item.quantity;
-        this.totalPrice -= item.quantity * item.price;
     }
     storeWithExpiration.set(this.name, this)
 }
@@ -64,8 +54,6 @@ ShoppingCart.prototype.changeQuantity = function(id, toChange) {
             this.removeItemByIndex(index);
         } else {
             item.quantity = itemQ + toChange;
-            this.totalCount += toChange;
-            this.totalPrice += toChange * item.price;
             storeWithExpiration.set(this.name, this);
         }
     }
