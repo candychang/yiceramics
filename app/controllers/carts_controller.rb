7dd66707f6f494ce3f65415933ab1ba05642c386
Candy Chang <candyichang@gmail.com>
2016-12-20 02:59:01 +0000

    inventory confirmation works

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,16 +7,26 @@ respond_to :json
     error_messages = []
     
     for item in item_list do
-      new_item = CartItem.where(cart_id: current_cart.id).find_or_initialize_by(work_id: item[:work_id])
-      new_item.work_id = item[:work_id]
-      new_item.cart_id = current_cart.id
+      new_item = CartItem.where(cart_id: current_cart.id).where(work_id: item[:work_id]).first
+      stock_left = Work.get_stock(item[:work_id])
+      if(new_item)
+        toChange = item[:quantity] - new_item.quantity
+        if stock_left == 0
+          stock_left = new_item.quantity
+        end
+      else
+        toChange = item[:quantity]
+        new_item = CartItem.new
+        new_item.cart_id = current_cart.id
+        new_item.work_id = item[:work_id]
+      end
       
-      if Work.check_stock(item[:work_id], item[:quantity])
+      
+      if Work.check_stock(item[:work_id], toChange)
         new_item.quantity = item[:quantity]
         new_item.save
         new_list << item
       else
-        stock_left = Work.get_stock(item[:work_id])
         if stock_left > 0 
           new_item.quantity = stock_left
           item[:quantity] = stock_left
@@ -24,9 +34,14 @@ respond_to :json
           new_item.save
           error_messages << "Only #{stock_left} left of #{item[:name]}. The quantity in your cart has been adjusted."
         else
+          new_item.destroy
+          item[:quantity] = 0
+          new_list << item
           error_messages << "Unfortunately, #{item[:name]} is out of stock."
+          errors = true
         end
       end
+      
     end
     
     render json: ({items: new_list, errors: error_messages}), status: 200
